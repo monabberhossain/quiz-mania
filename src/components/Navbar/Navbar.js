80b383ad19081842acdd3da70c35ac08cd0f9762
monabberhossain <mn2world@gmail.com>
2022-10-11 12:02:22 +0600

    installing dependencies and create navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const NavBar = () => {
+    return (
+        <div>
+            <Navbar expand="md lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Quiz Mania</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me"
+                            style={{ height: "auto" }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
